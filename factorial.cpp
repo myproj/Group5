3a7d4e119af73cd060afa7749cac972f00d2dfcb
Siddhant Kumar <saytosid@gmail.com>
2016-08-29 16:07:13 +0530

    Check for negetive numbers added

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
 
-int factorial(int n){
+int factorial(int n)
+{
+	if(N<0){
+	cout<<"Enter positive number\n";
+	cin>>n;
+	factorial(n)
+}
     if(n!=1){
 	return(n * factorial(n-1));
     }
